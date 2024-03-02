272d462f686d3740981dd13c9b05e45e0500995a
riley <rilesdou@gmail.com>
2024-03-01 21:44:34 -0500

    add proj2 revised

--- project_2/SDLProject/main.cpp
+++ project_2/SDLProject/main.cpp
@@ -33,6 +33,9 @@ const float BG_RED = 0.1922f,
             BG_GREEN = 0.9059f,
             BG_OPACITY = 0.75f;
 
+const float MILLISECONDS_IN_SECOND = 1000.0;
+const float MINIMUM_COLLISION_DISTANCE = 0.85f;
+
 const int VIEWPORT_X = 0,
           VIEWPORT_Y = 0,
           VIEWPORT_WIDTH = WINDOW_WIDTH,
@@ -40,8 +43,13 @@ const int VIEWPORT_X = 0,
 
 const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
            F_SHADER_PATH[] = "shaders/fragment_textured.glsl",
+           LEFT_TOTORO_FILEPATH[] = "left-totoro.png",
            RIGHT_TOTORO_FILEPATH[] = "right-totoro.png",
-           LEFT_TOTORO_FILEPATH[] = "left-totoro.png";
+           BALL1_FILEPATH[] = "soot.png",
+           BALL2_FILEPATH[] = "soot.png",
+           BALL3_FILEPATH[] = "soot.png",
+           RIGHT_WIN_MSG_FILEPATH[] = "right-win.png",
+           LEFT_WIN_MSG_FILEPATH[] = "left-win.png";
 
 const int NUMBER_OF_TEXTURES = 1; // to be generated, that is
 const GLint LEVEL_OF_DETAIL = 0;  // base image level; Level n is the nth mipmap reduction image
@@ -54,11 +62,53 @@ ShaderProgram g_shader_program;
 glm::mat4   g_view_matrix,
             g_right_totoro_model_matrix,
             g_left_totoro_model_matrix,
+            g_ball1_model_matrix,
+            g_ball2_model_matrix,
+            g_ball3_model_matrix,
+            g_left_win_msg_model_matrix,
+            g_right_win_msg_model_matrix,
             g_projection_matrix,
             g_trans_matrix;
 
 GLuint  g_right_totoro_texture_id,
-        g_left_totoro_texture_id;
+        g_left_totoro_texture_id,
+        g_ball1_texture_id,
+        g_ball2_texture_id,
+        g_ball3_texture_id,
+        g_left_win_msg_texture_id,
+        g_right_win_msg_texture_id;
+
+bool g_right_totoro_turn1,
+     g_right_totoro_turn2 = false,
+     g_right_totoro_turn3 = true;
+
+// Req 1: Two totoros on both sides
+glm::vec3 g_right_totoro_position = glm::vec3(4.0f, 0.0f, 0.0f);
+glm::vec3 g_right_totoro_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_left_totoro_position = glm::vec3(-4.0f, 0.0f, 0.0f);
+glm::vec3 g_left_totoro_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_ball1_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball2_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball3_position = glm::vec3(1.0f, 0.0f, 0.0f);
+
+glm::vec3 g_ball1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball2_movement = glm::vec3(-1.0f, -1.0f, 0.0f);
+glm::vec3 g_ball3_movement = glm::vec3(1.0f, 1.0f, 0.0f);
+
+const glm::vec3 scale = glm::vec3(2.0f, 2.0f, 1.0f);
+float g_speed = 2.0f;
+
+bool g_is_bot = false;
+glm::vec3 g_right_bot_movement = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float g_previous_ticks = 0.0f;
+
+bool g_right_totoro_win = false,
+     g_left_totoro_win = false;
+
+int g_number_of_balls = 1;
 
 GLuint load_texture(const char* filepath) {
     // STEP 1: Loading the image file
@@ -108,9 +158,6 @@ void initialise() {
     
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
     
-    g_right_totoro_model_matrix = glm::mat4(1.0f);
-    g_left_totoro_model_matrix = glm::mat4(1.0f);
-    
     g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
     
@@ -121,18 +168,271 @@ void initialise() {
     
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
     
-    g_right_totoro_texture_id = load_texture(RIGHT_TOTORO_FILEPATH);
+    // Load textures
+    // EC: From My Neighbor Totoro
     g_left_totoro_texture_id = load_texture(LEFT_TOTORO_FILEPATH);
+    g_right_totoro_texture_id = load_texture(RIGHT_TOTORO_FILEPATH);
+    g_ball1_texture_id = load_texture(BALL1_FILEPATH);
+    g_ball2_texture_id = load_texture(BALL2_FILEPATH);
+    g_ball3_texture_id = load_texture(BALL3_FILEPATH);
+    g_left_win_msg_texture_id = load_texture(LEFT_WIN_MSG_FILEPATH);
+    g_right_win_msg_texture_id = load_texture(RIGHT_WIN_MSG_FILEPATH);
+    
+    // Set model matrices
+    g_left_totoro_model_matrix = glm::mat4(1.0f);
+    g_right_totoro_model_matrix = glm::mat4(1.0f);
+    g_ball1_model_matrix = glm::mat4(1.0f);
+    g_ball2_model_matrix = glm::mat4(1.0f);
+    g_ball3_model_matrix = glm::mat4(1.0f);
+    g_left_win_msg_model_matrix = glm::mat4(1.0f);
+    g_right_win_msg_model_matrix = glm::mat4(1.0f);
+    
+    float x, y;
+    
+    // Set player who plays first as random
+    g_right_totoro_turn1 = (rand() % 100 > 50) ? true : false;
+    
+    // Set random movement
+    x = (g_right_totoro_turn1) ? 1.0f : -1.0f;
+    y = (rand() % 100 > 50) ? 1.0f : -1.0f;
+    g_ball1_movement = glm::vec3(x, y, 0.0f);
+    
+    g_left_win_msg_model_matrix = glm::scale(g_left_totoro_model_matrix, glm::vec3(5.0f, 5.0f, 1.0f));
+    g_right_win_msg_model_matrix = glm::scale(g_left_totoro_model_matrix, glm::vec3(5.0f, 5.0f, 1.0f));
     
     // enable blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-void process_input() {
+void process_input()
+{
+    
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_QUIT:
+            case SDL_WINDOWEVENT_CLOSE:
+                g_game_is_running = false;
+                break;
+                
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {   // Req 1
+                    case SDLK_UP:
+                        g_right_totoro_movement.y = 1.0f;
+                        break;
+                    case SDLK_DOWN:
+                        g_right_totoro_movement.y = -1.0f;
+                        break;
+                    case SDLK_w:
+                        g_left_totoro_movement.y = 1.0f;
+                        break;
+                    case SDLK_s:
+                        g_left_totoro_movement.y = -1.0f;
+                        break;
+                    case SDLK_t:
+                        g_is_bot = true;
+                        break;
+                    case SDLK_1:
+                        g_number_of_balls = 1;
+                        break;
+                    case SDLK_2:
+                        g_number_of_balls = 2;
+                        break;
+                    case SDLK_3:
+                        g_number_of_balls = 3;
+                        break;
+                    default:
+                        break;
+                }
+                
+            default:
+                break;
+        }
+    }
+    
+    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+    
+    if (key_state[SDL_SCANCODE_DOWN]) {
+        g_right_totoro_movement.y = -1.0f;
+    }
+    else if (key_state[SDL_SCANCODE_UP]) {
+        g_right_totoro_movement.y = 1.0f;
+    }
+    
+    if (key_state[SDL_SCANCODE_S]) {
+        g_left_totoro_movement.y = -1.0f;
+    }
+    else if (key_state[SDL_SCANCODE_W]) {
+        g_left_totoro_movement.y = 1.0f;
+    }
+    
+    if (glm::length(g_right_totoro_movement) > 1.0f) {
+            g_right_totoro_movement = glm::normalize(g_right_totoro_movement);
+        }
+    if (glm::length(g_left_totoro_movement) > 1.0f) {
+        g_left_totoro_movement = glm::normalize(g_left_totoro_movement);
+    }
+}
+
+// Checks if ball collides with one of the totoros
+bool did_collide(const glm::vec3& ball_position, bool turn) {
+    float x_right_distance = fabs(g_right_totoro_position.x + 0.05f - ball_position.x) - (MINIMUM_COLLISION_DISTANCE + MINIMUM_COLLISION_DISTANCE) / 2.0f;
+    float y_right_distance = fabs(g_right_totoro_position.y + 0.05f - ball_position.y) - (MINIMUM_COLLISION_DISTANCE + MINIMUM_COLLISION_DISTANCE) / 2.0f;
+    float x_left_distance = fabs(g_left_totoro_position.x + 0.05f - ball_position.x) - (MINIMUM_COLLISION_DISTANCE + MINIMUM_COLLISION_DISTANCE) / 2.0f;
+    float y_left_distance = fabs(g_left_totoro_position.y + 0.05f  - ball_position.y) - (MINIMUM_COLLISION_DISTANCE + MINIMUM_COLLISION_DISTANCE) / 2.0f;
+    
+    return (turn && x_right_distance < 0 && y_right_distance < 0) || (!turn && x_left_distance < 0 && y_left_distance < 0);
 }
 
 void update() {
+    
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND; // get the current number of ticks
+    float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
+    g_previous_ticks = ticks;
+    
+    // Req 3: If one of totoros win, stop the game
+    if (g_right_totoro_win || g_left_totoro_win) {
+        // Stop movement
+        g_right_totoro_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+        g_left_totoro_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+        g_ball1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+        g_ball2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+        g_ball3_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+        
+        // Return to original position
+        g_right_totoro_position = glm::vec3(4.0f, 0.0f, 0.0f);
+        g_left_totoro_position = glm::vec3(-4.0f, 0.0f, 0.0f);
+    }
+    
+    g_left_totoro_position += g_left_totoro_movement * g_speed * delta_time;
+    
+    // Control ball(s) movement
+    if (g_number_of_balls >= 1) {
+        g_ball1_position += g_ball1_movement * delta_time;
+    }
+    if (g_number_of_balls >= 2) {
+        g_ball2_position += g_ball2_movement * delta_time;
+    }
+    if (g_number_of_balls >= 3) {
+        g_ball3_position += g_ball3_movement * delta_time;
+    }
+    
+    // Control bot movement
+    if (g_is_bot) {
+        if (g_right_totoro_position.y >= 2.75f) {
+            g_right_bot_movement.y = -1.0f;
+        }
+        else if (g_right_totoro_position.y <= -2.75f) {
+            g_right_bot_movement.y = 1.0f;
+        }
+        
+        g_right_totoro_position += g_right_bot_movement * g_speed * delta_time;
+        
+    } else {
+        g_right_totoro_position += g_right_totoro_movement * g_speed * delta_time;
+    }
+    
+    // Req 1: Make sure that totoros don't go off border
+    if (g_right_totoro_position.y > 2.75f) {
+        g_right_totoro_position.y = 2.75f;
+    }
+    else if (g_right_totoro_position.y < -2.75f) {
+        g_right_totoro_position.y = -2.75f;
+    }
+    
+    if (g_left_totoro_position.y > 2.75f) {
+        g_left_totoro_position.y = 2.75f;
+    }
+    else if (g_left_totoro_position.y < -2.75f) {
+        g_left_totoro_position.y = -2.75f;
+    }
+    
+    // Reset model matrix
+    g_left_totoro_model_matrix = glm::mat4(1.0f);
+    g_right_totoro_model_matrix = glm::mat4(1.0f);
+    g_ball1_model_matrix = glm::mat4(1.0f);
+    g_ball2_model_matrix = glm::mat4(1.0f);
+    g_ball3_model_matrix = glm::mat4(1.0f);
+
+    // Req 1: Check if ball collides with totoros
+    if (did_collide(g_ball1_position, g_right_totoro_turn1)){
+        
+        g_ball1_movement.x *= -1.0f;
+        g_ball1_movement.y = (rand() % 100 > 50) ? -0.75f : 0.75f;
+        
+        g_right_totoro_turn1 = !g_right_totoro_turn1;
+    }
+    
+    if(did_collide(g_ball2_position, g_right_totoro_turn2)) {
+        g_ball2_movement.x *= -1.25f;
+        g_ball2_movement.y = (rand() % 100 > 50) ? -0.5f : 0.5f;
+        
+        g_right_totoro_turn2 = !g_right_totoro_turn2;
+    }
+    
+    
+    if(did_collide(g_ball3_position, g_right_totoro_turn3)) {
+        g_ball3_movement.x *= -1.25f;
+        g_ball3_movement.y = (rand() % 100 > 50) ? -1.75f : 1.75f;
+        
+        g_right_totoro_turn3 = !g_right_totoro_turn3;
+    }
+    
+    // Move totoros
+    g_left_totoro_model_matrix = glm::translate(g_left_totoro_model_matrix, g_left_totoro_position);
+    g_right_totoro_model_matrix = glm::translate(g_right_totoro_model_matrix, g_right_totoro_position);
+    
+    // Move ball(s)
+    if (g_number_of_balls >= 1) {
+        g_ball1_model_matrix = glm::translate(g_ball1_model_matrix, g_ball1_position);
+    }
+    if (g_number_of_balls >= 2) {
+        g_ball2_model_matrix = glm::translate(g_ball2_model_matrix, g_ball2_position);
+    }
+    if (g_number_of_balls >= 3) {
+        g_ball3_model_matrix = glm::translate(g_ball3_model_matrix, g_ball3_position);
+    }
+    
+    // Make totoros bigger
+    g_left_totoro_model_matrix = glm::scale(g_left_totoro_model_matrix, scale);
+    g_right_totoro_model_matrix = glm::scale(g_right_totoro_model_matrix, scale);
+    
+    // Req 2: Make balls bounce off of border of window
+    if (g_ball1_position.y > 3.45f) {
+        g_ball1_position.y = 3.45f;
+        g_ball1_movement.y *= -1.0f;
+    } else if (g_ball1_position.y < -3.45f) {
+        g_ball1_position.y = -3.45f;
+        g_ball1_movement.y *= -1.0f;
+    }
+    
+    if (g_ball2_position.y > 3.45f) {
+        g_ball2_position.y = 3.45f;
+        g_ball2_movement.y *= -1.0f;
+    } else if (g_ball2_position.y < -3.45f) {
+        g_ball2_position.y = -3.45f;
+        g_ball2_movement.y *= -1.0f;
+    }
+    
+    if (g_ball3_position.y > 3.45f) {
+        g_ball3_position.y = 3.45f;
+        g_ball3_movement.y *= -1.0f;
+    } else if (g_ball3_position.y < -3.45f) {
+        g_ball3_position.y = -3.45f;
+        g_ball3_movement.y *= -1.0f;
+    }
+    
+    // Set winners
+    if (g_ball1_position.x < -5.5f || g_ball2_position.x < -5.5f || g_ball3_position.x < -5.5f) {
+        g_right_totoro_win = true;
+    } else if (g_ball1_position.x > 5.5f || g_ball2_position.x > 5.5f || g_ball3_position.x > 5.5f) {
+        g_left_totoro_win = true;
+    }
+    
 }
 
 
@@ -167,6 +467,23 @@ void render() {
     draw_object(g_right_totoro_model_matrix, g_right_totoro_texture_id);
     draw_object(g_left_totoro_model_matrix, g_left_totoro_texture_id);
     
+    if (g_number_of_balls >= 1) {
+        draw_object(g_ball1_model_matrix, g_ball1_texture_id);
+    }
+    if (g_number_of_balls >= 2) {
+        draw_object(g_ball2_model_matrix, g_ball2_texture_id);
+    }
+    if (g_number_of_balls >= 3) {
+        draw_object(g_ball3_model_matrix, g_ball3_texture_id);
+    }
+    
+    // EC: End game message
+    if (g_left_totoro_win) {
+        draw_object(g_left_win_msg_model_matrix, g_left_win_msg_texture_id);
+    } else if (g_right_totoro_win) {
+        draw_object(g_right_win_msg_model_matrix, g_right_win_msg_texture_id);
+    }
+    
     // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
     glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
@@ -183,6 +500,8 @@ int main(int argc, char* argv[]) {
     
     while (g_game_is_running)
     {
+        process_input();
+        update();
         render();
     }
     
