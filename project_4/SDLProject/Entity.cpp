051c19691e30c8241c5ab73b41c2ebdedbfcb1a1
rilieo <rilesdou@gmail.com>
2024-03-29 00:40:31 -0400

    add statement

--- project_4/SDLProject/Entity.cpp
+++ project_4/SDLProject/Entity.cpp
@@ -1,3 +1,13 @@
+/*
+* Author: Riley Dou
+* Assignment: Rise of the AI
+* Date due: 2024-03-30, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+*/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
--- project_4/SDLProject/Map.cpp
+++ project_4/SDLProject/Map.cpp
@@ -1,7 +1,7 @@
 /*
 * Author: Riley Dou
 * Assignment: Rise of the AI
-* Date due: 2024-03-29, 11:59pm
+* Date due: 2024-03-30, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
