930bb676f07b0fab626da340b4164428169349d6
rilieo <rilesdou@gmail.com>
2024-04-09 00:58:45 -0400

    delete copies

--- project_4 copy/SDLProject/LevelB.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  LevelB.cpp
-//  SDLProject
-//
-//  Created by Riley Dou on 4/7/24.
-//  Copyright © 2024 ctg. All rights reserved.
-//
-
-#include <stdio.h>
--- project_4 copy/SDLProject/MenuScreen.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MenuScreen.cpp
-//  SDLProject
-//
-//  Created by Riley Dou on 4/7/24.
-//  Copyright © 2024 ctg. All rights reserved.
-//
-
-#include <stdio.h>
